ee350f476fd7c8b28866dc605db27b38471c2541
YASHU YADAV A <100356083+Yashu9353@users.noreply.github.com>
2024-09-05 17:43:18 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -177,7 +177,7 @@ const PlaceOrder = () => {
                     </div>
 
                     <div className='w-full text-end mt-8'>
-                        <button type='submit' className='bg-black text-white px-16 py-3 text-sm'>PLACE ORDER</button>
+                        <button type='submit' className='bg-black text-gray-500 px-16 py-3 text-sm'>PLACE ORDER</button>
                     </div>
                 </div>
             </div>
