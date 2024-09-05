708779f2304c64737b3640929d7f095c86a58644
YASHU YADAV A <100356083+Yashu9353@users.noreply.github.com>
2024-09-05 17:59:22 +0530

    Update Product.jsx

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -64,11 +64,11 @@ const Product = () => {
               <p>Select Size</p>
               <div className='flex gap-2'>
                 {productData.sizes.map((item,index)=>(
-                  <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
+                  <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-500 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
                 ))}
               </div>
           </div>
-          <button onClick={()=>addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          <button onClick={()=>addToCart(productData._id,size)} className='bg-black text-gray-500 px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
           <hr className='mt-8 sm:w-4/5' />
           <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
               <p>100% Original product.</p>
