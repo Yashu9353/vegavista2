45364f8e36ebc0caec28ecaada418be8bd3e0361
YASHU YADAV A <100356083+Yashu9353@users.noreply.github.com>
2024-09-05 15:27:20 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -3,14 +3,14 @@ import logo31 from './logo31.png';
 import add_icon from './add_icon.png';
 import order_icon from './order_icon.png';
 import upload_area from './upload_area.png';
-import vega from './vega.png';
+import vegaa from './vegaa.png';
 import parcel_icon from './parcel_icon.svg';
 
 export const url="https://vegavista2.onrender.com"
 export const assets = {
   logo,
   add_icon,
-  vega,
+  vegaa,
   logo31,
   order_icon,
   upload_area,
