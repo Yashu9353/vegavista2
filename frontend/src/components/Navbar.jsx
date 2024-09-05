5c54f35394500490fba0c6aae122d8ace2f46d8c
YASHU YADAV A <100356083+Yashu9353@users.noreply.github.com>
2024-09-05 17:26:58 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -68,7 +68,7 @@ const Navbar = () => {
           {/* Dropdown Menu */}
           {token && (
             <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
-              <div className="flex flex-col gap-2 w-36 py-3 px-5  bg-slate-100 text-gray-500 rounded">
+              <div className="flex flex-col gap-2 w-36 py-3 px-5  bg-gray-500 text-gray-500 rounded">
                 <p className="cursor-pointer hover:text-black">My Profile</p>
                 <p
                   onClick={() => navigate('/orders')}
@@ -85,7 +85,7 @@ const Navbar = () => {
         </div>
         <Link to="/cart" className="relative">
           <img src={assets.cart2} className="w-5 min-w-5" alt="" />
-          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-gray-500 aspect-square rounded-full text-[8px]">
             {getCartCount()}
           </p>
         </Link>
@@ -99,7 +99,7 @@ const Navbar = () => {
 
       {/* Sidebar menu for small screens */}
       <div
-        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-gray-500 transition-all ${
           visible ? 'w-full' : 'w-0'
         }`}
       >
