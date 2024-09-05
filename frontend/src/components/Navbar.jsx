2e889ea2d3dcd8caf1c9933782da9f615e0f82cc
YASHU YADAV A <100356083+Yashu9353@users.noreply.github.com>
2024-09-05 15:18:16 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to="/">
-        <img src={assets. vega} className="w-36" alt="" />
+        <img src={assets.vegaa} className="w-36" alt="" />
       </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
