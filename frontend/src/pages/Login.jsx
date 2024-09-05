816bb26e81966de5532bb615ae00ec919b1dd08b
YASHU YADAV A <100356083+Yashu9353@users.noreply.github.com>
2024-09-05 17:40:36 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -67,7 +67,7 @@ const Login = () => {
               : <p onClick={()=>setCurrentState('Login')} className=' cursor-pointer'>Login Here</p>
             }
         </div>
-        <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
+        <button className='bg-black text-gray-500 font-light px-8 py-2 mt-4'>{currentState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
     </form>
   )
 }
